83b1320f53df57b98633d4f3a0573f38b7d9ec57
samwel-chege <samwelchegeh09@gmail.com>
2021-06-26 12:07:22 +0300

    make changes

---
+++ JS/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("").mouseover(function(){
+        $("").show();
+    }).mouseout(function(){
+        $("").hide();
+    });
+    $("").mouseover(function(){
+        $("").show();
+    }).mouseout(function(){
+        $("").hide();
+    });
+    $("").mouseover(function(){
+        $("").show();
+    }).mouseout(function(){
+        $("").hide();
+    });
+
+})
